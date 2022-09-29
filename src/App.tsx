bd1512c1503acd9cf13337fe51f922f326e126a2
mat3ed18 <mathiaspedro9@gmail.com>
2022-09-29 03:13:06 +0000

    adicionando o jquery

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
+// import $ from 'jquery';
 
 export default function App() {
+    
+    useEffect(() => {
+        
+    }, []);
+
     return (
         <div className="App">
             <div className="App-header">
-                <h1>Teste Simix</h1><hr/>
-                <button className='btn btn-success'>teste</button>
+                <h1>Teste Simix dfg</h1><hr/>
+                <button className='btn btn-primary'>teste</button>
             </div>
         </div>
     );
