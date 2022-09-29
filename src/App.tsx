95431e3121e8ba2e6be77f28a965ee0bf85144ad
mat3ed18 <mathiaspedro9@gmail.com>
2022-09-29 03:50:07 -0300

    finalização, testes e melhorias no design

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,58 @@
-import { useEffect } from 'react';
+import { Tooltip } from 'bootstrap';
+import { useEffect, useState } from 'react';
 import './App.css';
-// import $ from 'jquery';
 
 export default function App() {
-    
+
+    const [table, setTable] = useState([<sup key={1} className="mt-3">Clique no botão e veja os números</sup>]);
+
+    const defaultMessage = (num: number, tx: string | undefined) => {
+        switch (tx) {
+            case "X":
+                return `O número <b>${num}</b> é divisível por <b>3</b>, <br> <b>${num}</b> / <b>3</b> = <b>${num / 3}</b>`;
+            case "Y":
+                return `O número <b>${num}</b> é divisível por <b>5</b>, <br> <b>${num}</b> / <b>5</b> = <b>${num / 5}</b>`;
+            case "Z":
+                return `O número <b>${num}</b> é divisível por <b>3</b> e <b>5</b>, <br> <u><b>${num}</b> / <b>3</b> = <b>${num / 3}</b></u> e <u><b>${num}</b> / <b>5</b> = <b>${num / 5}</b></u>`;
+            default:
+                return `${num}`;
+        }
+    }
+
+    const getNumbers = () => {
+        const data = [];
+        let num = 1;
+
+        for (let i = 0; i < 20; i++) {
+            const row = [];
+            for (let e = 0; e < 10; e++) {
+                let tx;
+
+                if (num % 3 === 0) tx = "X";
+                if (num % 5 === 0) tx = "Y";
+                if (num % 3 === 0 && num % 5 === 0) tx = "Z";
+
+                row.push(<td key={e} data-bs-toggle="tooltip" data-bs-html="true" data-bs-title={defaultMessage(num, tx)} id={"lbl" + tx}>{(tx != null) ? tx : num}</td>);
+                num++;
+            }
+            data.push(<tr key={i}>{row}</tr>);
+        }
+
+        setTable([<table id="tabela" key={1} className="table table-sm table-light shadow"><tbody>{data}</tbody></table>]);
+
+    }
+
     useEffect(() => {
-        
-    }, []);
+        const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        tooltipTriggerList.map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
+    });
 
     return (
-        <div className="App">
-            <div className="App-header">
-                <h1>Teste Simix dfg</h1><hr/>
-                <button className='btn btn-primary'>teste</button>
+        <div className="app">
+            <div className="app-header">
+                <h1 className="mb-4">Teste Simix</h1>
+                <button onClick={getNumbers} className='btn btn-primary shadow'>mostrar</button><hr />
+                {table}
             </div>
         </div>
     );
