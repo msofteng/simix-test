ac7d526c548783d782efda901dc754e9900ffe8f
mat3ed18 <mathiaspedro9@gmail.com>
2022-09-28 23:26:36 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <h1>Teste Simix</h1><hr/>
+                <button className='btn btn-success'>teste</button>
+            </div>
+        </div>
+    );
+}
